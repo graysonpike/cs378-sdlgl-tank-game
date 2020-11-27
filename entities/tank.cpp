5d0f19ff73dd6e80cbdc8b71cf508f07a82ddadd
Grayson Pike <graysonpike@gmail.com>
2020-11-27 15:55:46 -0600

    Add firing animation and sounds

--- entities/tank.cpp
+++ entities/tank.cpp
@@ -21,24 +21,43 @@ Tank::Tank(Scene *scene, float x, float y) :
     textures["hull"] = resources->get_texture("sherman_hull");
     textures["turret"] = resources->get_texture("sherman_turret");
     textures["barrel"] = resources->get_texture("sherman_barrel");
+
+    fire_sound = resources->get_sound("sherman_fire");
 }
 
 
 void Tank::drive(bool direction, bool turn_left, bool turn_right) {}
 
-void Tank::rotate_turret(bool direction) {}
+void Tank::rotate_turret(bool direction) {
+    turret_angle += turret_speed * (direction ? 1 : -1);
+}
 
-void Tank::fire() {}
+void Tank::fire() {
+    if (!reload_timer.is_done()) {
+        return;
+    }
+    // Fire gun
+    reload_timer.reset();
+    recoil_timer.reset();
+    fire_sound.play();
+}
 
 void Tank::update() {}
 
 void Tank::render() {
+
     textures["hull"].draw(scene, x, y, hull_angle);
-    std::pair<int, int> barrel_offset_position = rotate_vector(barrel_offset, turret_angle);
+
+    // Handle recoil on gun barrel
+    float recoil_offset = 0.0f;
+    if (!recoil_timer.is_done()) {
+        recoil_offset = (recoil_timer.remaining()) * gun_recoil_amount;
+    }
+    std::pair<int, int> barrel_offset_position = rotate_vector(barrel_offset - recoil_offset, turret_angle);
     textures["barrel"].draw(scene, barrel_offset_position.first + x, barrel_offset_position.second + y, turret_angle);
+
     std::pair<int, int> turret_offset_position = rotate_vector(turret_offset, hull_angle);
     textures["turret"].draw(scene, turret_offset_position.first + x, turret_offset_position.second + y, turret_angle);
-
 
     // Debug Graphics
     SDL_Renderer *renderer = scene->get_graphics()->get_renderer();
--- entities/tank.h
+++ entities/tank.h
@@ -4,15 +4,17 @@
 #include <SDL2/SDL.h>
 #include <sdlgl/game/scene.h>
 #include <sdlgl/game/physical_entity.h>
+#include <sdlgl/game/timer.h>
 #include <sdlgl/graphics/texture.h>
 
 class Tank : public PhysicalEntity {
 
     std::map<std::string, Texture> textures;
-    float reload_timer = 0.0f;
+    Sound fire_sound;
+    Timer reload_timer = Timer(3.0f);
+    Timer recoil_timer = Timer(1.0f);
     float turret_angle = 0.0f;
     float hull_angle = 0.0f;
-    float reload_time = 1.0f;
     float driving_speed = 40.0f;
     float turning_speed = 30.0f;
     float turret_speed = 30.0f;
@@ -20,15 +22,15 @@ class Tank : public PhysicalEntity {
     static const int turret_offset = 0;
     static const int barrel_offset = 40;
 
-    void drive(bool direction, bool turn_left, bool turn_right);
-    void rotate_turret(bool direction);
-    void fire();
-
 public:
 
     Tank(Scene *scene, float x, float y);
     void update();
     void render();
+
+    void drive(bool direction, bool turn_left, bool turn_right);
+    void rotate_turret(bool direction);
+    void fire();
 
 };
 
